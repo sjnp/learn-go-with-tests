9020fc42c34cfcf4215eab687c13b9f24f2e59d8
sjnp <supawet.j@gmail.com>
2025-03-18 01:06:17 +0700

    Finish integers

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -1,1 +1,35 @@
-package hello_world
+package main
+
+import "fmt"
+
+const (
+	englishHelloPrefix  = "Hello, "
+	spanishHelloPrefix  = "Hola, "
+	japaneseHelloPrefix = "Konichiwa, "
+
+	spanish  = "Spanish"
+	japanese = "Japanese"
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return getHelloPrefix(language) + name
+}
+
+func getHelloPrefix(language string) string {
+	switch language {
+	case spanish:
+		return spanishHelloPrefix
+	case japanese:
+		return japaneseHelloPrefix
+	default:
+		return englishHelloPrefix
+	}
+}
+
+func main() {
+	fmt.Println(Hello("World", ""))
+}
