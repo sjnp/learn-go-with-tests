4daab8d07a1b101907c9e75ee7b96656bd16b0c9
sjnp <supawet.j@gmail.com>
2025-10-23 16:27:44 +0700

    Hello go again

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/sjnp/learn-go-with-tests
 
-go 1.24.1
+go 1.25.3
--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,34 +2,6 @@ package main
 
 import "fmt"
 
-const (
-	englishHelloPrefix  = "Hello, "
-	spanishHelloPrefix  = "Hola, "
-	japaneseHelloPrefix = "Konichiwa, "
-
-	spanish  = "Spanish"
-	japanese = "Japanese"
-)
-
-func Hello(name string, language string) string {
-	if name == "" {
-		name = "World"
-	}
-
-	return getHelloPrefix(language) + name
-}
-
-func getHelloPrefix(language string) string {
-	switch language {
-	case spanish:
-		return spanishHelloPrefix
-	case japanese:
-		return japaneseHelloPrefix
-	default:
-		return englishHelloPrefix
-	}
-}
-
 func main() {
-	fmt.Println(Hello("World", ""))
+	fmt.Println("Hello world!")
 }
